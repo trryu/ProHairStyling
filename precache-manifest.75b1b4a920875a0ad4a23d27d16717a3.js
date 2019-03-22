e1e27323fd88ade61bd21b20f0e5543a84112c5a
RubenRybnikk <RubenRybnikk@hotmail.com>
2019-03-22 09:52:31 -0700

    Updates

---
+++ precache-manifest.75b1b4a920875a0ad4a23d27d16717a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e39527a09256664e131c6b56059fb9a",
+    "url": "/ProHairStyling/static/media/card.1e39527a.png"
+  },
+  {
+    "revision": "a8760bc2c2383057cc34",
+    "url": "/ProHairStyling/static/js/runtime~main.a8760bc2.js"
+  },
+  {
+    "revision": "37d15e021de140c19d28",
+    "url": "/ProHairStyling/static/js/main.37d15e02.chunk.js"
+  },
+  {
+    "revision": "7f14b911b91fc2cc7c92",
+    "url": "/ProHairStyling/static/js/2.7f14b911.chunk.js"
+  },
+  {
+    "revision": "37d15e021de140c19d28",
+    "url": "/ProHairStyling/static/css/main.0c593b5e.chunk.css"
+  },
+  {
+    "revision": "7f14b911b91fc2cc7c92",
+    "url": "/ProHairStyling/static/css/2.bc7f1611.chunk.css"
+  },
+  {
+    "revision": "e7d1a72f98712d41e9f891faca53a122",
+    "url": "/ProHairStyling/index.html"
+  }
+];

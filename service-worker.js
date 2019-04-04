25ea1f91160ad03629c893c2318097cb41b9e830
RubenRybnikk <RubenRybnikk@hotmail.com>
2019-04-04 10:09:34 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ProHairStyling/precache-manifest.75b1b4a920875a0ad4a23d27d16717a3.js"
+  "/ProHairStyling/precache-manifest.55ef067323e37c1eccb2fcf44a711edb.js"
 );
 
 workbox.clientsClaim();
